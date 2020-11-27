d183be589391da7c0c4ce394a977e9f8802c06bc
Douglas Echela <douglas.casagrande01@etec.sp.gov.br>
2020-11-27 14:09:40 -0300

    mapa
    atividade assíncrona

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map: google.maps.Map;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
   constructor() {}
+
+  ionViewWillEnter(){
+    this.exibirMapa();
+  }
+
+  exibirMapa(){
+    const posicao = new google.maps.LatLng(-16.650340, -49.228618);
+    const opcoes = {
+      center: posicao,
+      zoom: 15,
+      disableDefaultUI: true
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
+    
+    new google.maps.Marker({
+      position: posicao,
+      map: this.map,
+      title: "Praça da Matriz",
+      animation: google.maps.Animation.BOUNCE
+    });
+  }
 
 }
